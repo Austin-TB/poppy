db887d57ba87921e331787509dd1fed5d6734a4f
Austin-TB <61232664+Austin-TB@users.noreply.github.com>
2024-07-28 16:41:39 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,17 +1,22 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
 class Scraper:
     @staticmethod
     def fetch_latest_version():
-        url = "https://www.techpowerup.com"
+        url = os.getenv('WEBSITE_URL')
+        classname = os.getenv('CLASS_NAME')
         
+        if not url or classname:
+            return "URL/CLASS_NAME not found in environment variables"
+
         try:
             response = requests.get(url)
             response.raise_for_status()
             
             soup = BeautifulSoup(response.text, 'html.parser')
-            drivers = soup.find_all(class_="s--drivers__driver s--drivers__driver--nvidia")
+            drivers = soup.find_all(class_=classname)
             
             if drivers:
                 latest_driver = drivers[0].text.strip()
