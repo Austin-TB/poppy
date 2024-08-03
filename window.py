abe54d1a609d4d3bc266bd086f237837a1e5f0b6
Austin-TB <austintbabu@gmail.com>
2024-08-03 17:14:01 +0530

    Download driver

--- window.py
+++ window.py
@@ -1,14 +1,17 @@
 import tkinter as tk
 from scraper import Scraper
 from local import Local
+import webbrowser
 
 class DriverUpdateChecker:
     def __init__(self):
         self.root = tk.Tk()
+        self.root.iconbitmap("icon.ico")
         self.root.title("Driver Update Checker")
-        self.root.geometry("400x200")
+        self.root.geometry("400x250")
 
         self.setup_ui()
+        self.latest_version_number = None
 
     def setup_ui(self):
         label = tk.Label(self.root, text="Check for Graphics Driver Updates", font=("Arial", 14))
@@ -23,21 +26,33 @@ class DriverUpdateChecker:
         self.latest_version_label = tk.Label(self.root, text="Latest Version: Not Checked", font=("Arial", 12))
         self.latest_version_label.pack(pady=5)
 
+        self.download_button = tk.Button(self.root, text="Download Latest Driver", command=self.download_driver)
+        self.download_button.pack(pady=10)
+        self.download_button.pack_forget()
+
     def check_update(self):
         current_version = Local.get_graphics_driver_version()
+        # current_version = "47111"
         latest_version = Scraper.fetch_latest_version()
 
         self.current_version_label.config(text=f"Current Version: {current_version[:3]}.{current_version[3:]}")
         
         if latest_version.startswith("Error"):
             self.latest_version_label.config(text=f"Latest Version: {latest_version}")
+            self.download_button.pack_forget()
         else:
-            latest_version_number = latest_version.split()[2]  # Assuming format "NVIDIA GeForce 560.70 WHQL"
+            self.latest_version_number = latest_version.split()[2]
             
-            if current_version !=  latest_version_number.replace('.', ''):
-                self.latest_version_label.config(text=f"Latest Version: {latest_version_number} (Update Available)")
+            if current_version != self.latest_version_number.replace('.', ''):
+                self.latest_version_label.config(text=f"Latest Version: {self.latest_version_number} (Update Available)")
+                self.download_button.pack()
             else:
-                self.latest_version_label.config(text=f"Latest Version: {latest_version_number} (Up to Date)")
+                self.latest_version_label.config(text=f"Latest Version: {self.latest_version_number} (Up to Date)")
+                self.download_button.pack_forget()
+    def download_driver(self):
+        if self.latest_version_number:
+            url = f"https://in.download.nvidia.com/Windows/{self.latest_version_number}/{self.latest_version_number}-notebook-win10-win11-64bit-international-dch-whql.exe"
+            webbrowser.open(url)
 
     def run(self):
         self.root.mainloop()
